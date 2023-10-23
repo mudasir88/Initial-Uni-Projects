192eb650832d55e4bc9243d385b03563b304cd9e
Mudasir Ahmad <mudasirahmad1031@gmail.com>
2023-10-24 02:17:18 +0500

    Add files via upload

---
+++ sum 2 numbers by taking input.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int
+main ()
+{
+ cout<< " ENTER NO. 1 = ";
+ int num1;
+ cin>> num1  ;
+ 
+ cout<< " ENTER NO. 2 = ";
+ int num2 ;
+ cin>> num2;
+ cout<< endl;
+ cout<< " SUM = ";
+ cout<< num1 + num2 ;
+
+  return 0;
+}
