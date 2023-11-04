c1bcb7c2dc3e89961eb0e010be1e837654989f14
Mudasir Ahmad <mudasirahmad1031@gmail.com>
2023-11-04 17:35:03 +0500

    Add files via upload

---
+++ Int Float usage.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int m;
+    m=20;
+    float d;
+    d= 4.5;
+    
+    cout<< m*d ;
+
+    return 0;
+}
